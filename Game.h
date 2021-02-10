0da9e342f33a62d60d558d99768c7b57a33ae2dc
Gregório <30591064+gregorio1212@users.noreply.github.com>
2021-02-10 14:50:56 +0100

    Update Game.h

--- Game.h
+++ Game.h
@@ -8,19 +8,19 @@ class Game
 {
 private:
 	char m_board[8][8];
-	int m_disk_counter;			//limit up to 62
+	int m_disk_counter;			//limit up to 60
 	char m_whose_turn;			//B -> Black, W -> White
 	bool m_correctInput;		// check input when asking to play again
 	bool m_end;	
 	char m_playAgain;
-	char m_opponent;			//used when checking surroundings of free slots
+	char m_opponent;			
 	bool m_machine_check;
-	bool m_moves_available;
+	bool m_move_available;
 	int m_if_two_end;
-	//flipAllDirections variables, i_c_ represent x changes and j_c_ represents y changes
-	int i_c_;
-	int j_c_;
-	int if_flip_equals_10;
+	int m_x_path;
+	int m_y_path;
+	int m_temp;
+	int m_if_flip_equals_10;
 public:
 	Game();
 	void InitGame();
@@ -29,12 +29,12 @@ public:
 	bool freeSlot(int x, int y)const;
 	char getWhoseTurn() const { return m_whose_turn; }
 	int getDiskCounter() const { return m_disk_counter; }
+	void allowedSlotLoop(int x_, int y_, int i_c, int j_c, int lim_i, int lim_j);
 	bool allowedSlot(int x, int y);
 	bool allowedTurn();
 	int flipAllDirections(int x, int y, int i_c, int j_c);
 	void flipWhoseTurnAndOpponent();
-	void oneLessDiskGame() { m_disk_counter++; }
-	void settingNewDisk(int x, int y) { m_board[x][y] = m_whose_turn; }
+	void settingNewDisk(int x, int y);
 	void printBoard()const;
 	bool noPossibleMove();
 	bool getEnd() const { return m_end; }
