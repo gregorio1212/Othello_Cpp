96dd9c9223c4566453e9cbd957818a836bc10a43
Gregório <30591064+gregorio1212@users.noreply.github.com>
2021-02-10 14:47:20 +0100

    main.cpp possibly final version

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main() {
         othello.Info();
 
         //while the slot is not allowed or not free, keep asking the user for input
-        while (!allowed || !(othello.freeSlot(user_input_x, user_input_y))) {
+        while (!allowed) {
             user_input_x = -1;
             user_input_y = -1;
             while (!othello.goodInput(user_input_x, user_input_y)) {//cheking if it is inside the range of 0 to 7 index 
@@ -27,40 +27,32 @@ int main() {
                 cout << "Write the y coord you want to place your disk:\n";
                 cin >> user_input_y;
             }
-            allowed = othello.allowedSlot(user_input_x, user_input_y);
+            if (othello.freeSlot(user_input_x, user_input_y)) { allowed = othello.allowedSlot(user_input_x, user_input_y); }
         }
+        allowed = false;
 
-        //after being sure it's a good place, set the disk on that place
-        othello.settingNewDisk(user_input_x, user_input_y); 
-        othello.oneLessDiskGame(); 
-
+        othello.settingNewDisk(user_input_x, user_input_y);
         //keeping record of how many disks each player still has
         if (othello.getWhoseTurn() == 'B') { black.oneLessDisk();}
         else { white.oneLessDisk();}
 
-        // if we have reached 62 disks on the board we avoid the normal procedure and set the end
-        if (othello.getDiskCounter() < 62) {
-            //flip the player and the opponent for the next turn
+        // if we have reached 60 disks on the board we avoid the normal procedure and set the end
+        if (othello.getDiskCounter() <= 60) {
             othello.flipWhoseTurnAndOpponent();
-            // check for possible moves for the players
-            if (othello.noPossibleMove()) {
-                othello.setEnd(true);
-            }
+            if (othello.noPossibleMove()) { othello.setEnd(true); }
         }
         else { othello.setEnd(true); }
 
-        //showing result and asking if the player wants to play again
+        //showing results and asking if the player wants to play again
         if (othello.getEnd()) {
             othello.theWinnerIs();
-            othello.playAgain();
+            black.resetDisks();
+            white.resetDisks();
         }
-        else {//checking if the player needs disks to play
-            if (othello.getWhoseTurn() == 'B' && black.getDisksAvailable() == 0) {
-                black.giveMeOneDisk(white);
-            }
-            else if (othello.getWhoseTurn() == 'W' && white.getDisksAvailable() == 0) {
-                white.giveMeOneDisk(black);
-            }
+        //checking if the player needs disks from the opponent to play
+        else {
+            if (othello.getWhoseTurn() == 'B' && black.getDisksAvailable() == 0) { black.giveMeOneDisk(white); }
+            else if (othello.getWhoseTurn() == 'W' && white.getDisksAvailable() == 0) { white.giveMeOneDisk(black); }
         }  
     }
 }
