75097039d1c64d2799db8d2c8eadcbf3215d0b24
Gregório <30591064+gregorio1212@users.noreply.github.com>
2021-02-10 14:51:36 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -9,10 +9,10 @@ private:
 	int m_n_disks_available;
 	int m_color;				//WHITE == 1 BLACK == 2
 public:
-	Player(int color) : m_color{color}, m_n_disks_available { 29 } {}
+	Player(int color) : m_color{color}, m_n_disks_available { 28 } {}
 	int getDisksAvailable() const { return m_n_disks_available; }
 	void oneLessDisk();
 	void giveMeOneDisk(Player& giving);
+	void resetDisks() { m_n_disks_available = 28; }
 	int getColor() const { return m_color; }
 };
-
